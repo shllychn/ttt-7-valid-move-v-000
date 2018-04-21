a3ca8b28e3612df4f3ff077261b0547b51f7ba30
Shelly Chen <shellyjychen@gmail.com>
2018-04-21 07:47:16 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if index == 0 || index == 1 || index == 2 || index == 3 || index == 4 || index == 5 || index == 6 || index == 7 || index == 8 and position_taken?(board, index) == false
     true
 
-  elsif index > 8 || position_taken == true
+  elsif index > 8 || position_taken?(board, index) == true
     false
   end
 
